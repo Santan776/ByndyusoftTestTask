d82d6f67a8d6e973e162a89ebf606b357fee2683
Santan776 <edwardbbb17@gmail.com>
2025-05-06 15:45:24 +0300

    tests

---
+++ ByndyusoftTestTask.Tests/UnitTest.cs
@@ -0,0 +1,51 @@
+﻿namespace ByndyusoftTestTask.Tests;
+
+[TestFixture]
+public class ArrayUtilsTests
+{
+    [Test]
+    public void SumOfTwoSmallest_NormalArray_ReturnsCorrectSum()
+    {
+        int[] numbers = { 4, 0, 3, 19, 492, -10, 1 };
+        
+        var result = ArrayUtils.SumOfTwoSmallest(numbers);
+        
+        Assert.That(result, Is.EqualTo(-10)); // -10 + 0 = -10
+    }
+
+    [Test]
+    public void SumOfTwoSmallest_DuplicateValues_ReturnsCorrectSum()
+    {
+        int[] numbers = { 5, 5, 10, 5, 20 };
+        
+        var result = ArrayUtils.SumOfTwoSmallest(numbers);
+        
+        Assert.That(result, Is.EqualTo(10)); // 5 + 5 = 10
+    }
+
+    [Test]
+    public void SumOfTwoSmallest_TwoElementsOnly_ReturnsCorrectSum()
+    {
+        int[] numbers = { 7, 3 };
+        
+        var result = ArrayUtils.SumOfTwoSmallest(numbers);
+        
+        Assert.That(result, Is.EqualTo(10)); // 3 + 7 = 10
+    }
+
+    [Test]
+    public void SumOfTwoSmallest_SingleElement_ThrowsArgumentException()
+    {
+        int[] numbers = { 5 };
+        
+        Assert.Throws<ArgumentException>(() => ArrayUtils.SumOfTwoSmallest(numbers));
+    }
+
+    [Test]
+    public void SumOfTwoSmallest_NullArray_ThrowsArgumentException()
+    {
+        int[] numbers = null;
+        
+        Assert.Throws<ArgumentException>(() => ArrayUtils.SumOfTwoSmallest(numbers));
+    }
+}
