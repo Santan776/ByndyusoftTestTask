80520977ac2ec62c599b255175ba747d948f54d2
Santan776 <edwardbbb17@gmail.com>
2025-05-06 12:44:10 +0300

    array utils and unit tests

---
+++ ByndyusoftTestTask/ArrayUtils.cs
@@ -0,0 +1,32 @@
+﻿namespace ByndyusoftTestTask;
+
+using System;
+
+public static class ArrayUtils
+{
+    public static int SumOfTwoSmallest(int[] numbers)
+    {
+        if (numbers == null || numbers.Length < 2)
+        {
+            throw new ArgumentException("Массив должен содержать как минимум 2 элемента");
+        }
+
+        var smallest = int.MaxValue;
+        var secondSmallest = int.MaxValue;
+
+        foreach (var number in numbers)
+        {
+            if (number < smallest)
+            {
+                secondSmallest = smallest;
+                smallest = number;
+            }
+            else if (number < secondSmallest)
+            {
+                secondSmallest = number;
+            }
+        }
+
+        return smallest + secondSmallest;
+    }
+}
---
+++ ByndyusoftTestTask/Program.cs
@@ -0,0 +1,8 @@
+﻿using ByndyusoftTestTask;
+
+Console.WriteLine("lfg");
+int[] numbers = { 4, 0, 3, 19, 492, -10, 1 };
+
+var result = ArrayUtils.SumOfTwoSmallest(numbers);
+
+Console.WriteLine($"Result of this complex task is: {result}");
